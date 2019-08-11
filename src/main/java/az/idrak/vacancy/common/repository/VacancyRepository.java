698aba183f06d5cfa4fd9bcf40600e430b68b88b
murattiooo <murattiooo23@gmail.com>
2019-08-12 00:45:54 +0400

    Update 12.08.19

--- src/main/java/az/idrak/vacancy/common/repository/VacancyRepository.java
+++ src/main/java/az/idrak/vacancy/common/repository/VacancyRepository.java
@@ -2,11 +2,17 @@ package az.idrak.vacancy.common.repository;
 
 import az.idrak.vacancy.common.entity.VacancyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Repository
-public interface  VacancyRepository extends JpaRepository<VacancyEntity,Long> {
+public interface VacancyRepository extends JpaRepository<VacancyEntity, Long> {
 
-
+    @Query("select v from VacancyEntity v where :today between startDate  and endDate")
+    List<VacancyEntity> findActiveVacancies(@Param("today") LocalDate localDate);
 }
